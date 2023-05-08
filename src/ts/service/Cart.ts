9f3e5b6e410d1a7faa19ae8dfa743f5164f0262a
saff84 <saff84@mail.ru>
2023-05-08 10:14:50 +0300

    v2

--- src/ts/service/Cart.ts
+++ src/ts/service/Cart.ts
@@ -11,21 +11,24 @@ export default class Cart {
         return [...this._items]; 
     }
 
+    
     getTotal(): number {
-        let total: number = 0;
-        this._items.forEach((item) => {
-            total += item.price;
-        });
+
+        // this._items.reduce((summ,item)=> sum + item.price,0)
+        let total: number = this._items.reduce((sum : number,item: Buyable)=> sum + item.price,0);
+        // this._items.forEach((item) => {
+        //     total += item.price;
+        // });
         return total;
     }
 
     getDiscout(discout: number): number {
         let total: number = this.getTotal();
-        total = total - total * (discout / 100);
-        return total;
+        // total = total - total * (discout / 100);
+        return total - total * (discout / 100);
     }
 
     deleteId(id: number): void {
-        this._items = this._items.filter((item) => item.id !== id);
+        this._items = this._items.filter((item: Buyable) => item.id !== id);
     }
 }
